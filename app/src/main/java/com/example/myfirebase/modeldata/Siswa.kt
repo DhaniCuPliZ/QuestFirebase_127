f3e2bfa084d88f9600ab83b6fc0416cfc1ae6815
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2026-01-02 15:51:03 +0700

    Menambahkan model data Siswa dan DetailSiswa

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -1,2 +1,20 @@
 package com.example.myfirebase.modeldata
 
+
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+data class DetailSiswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
+
