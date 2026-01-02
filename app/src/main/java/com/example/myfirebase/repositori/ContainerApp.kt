dd21a35ad83f41cae0a9758c9d4ebee47fed2bd9
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2026-01-02 16:02:16 +0700

    enambahkan AplikasiDataSiswa sebagai Application class

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -12,3 +12,11 @@ class DefaultContainerApp : ContainerApp {
     }
 }
 
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
